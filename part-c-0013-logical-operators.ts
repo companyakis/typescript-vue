75b92971edbb4ffd8958a3fd4179009877ad9f69
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-10 20:57:05 +0300

    Create part-c-0013-logical-operators.ts

---
+++ part-c-0013-logical-operators.ts
@@ -0,0 +1,21 @@
+let x = true
+
+let y = true
+
+let z = false
+
+console.log(x && y)
+console.log(x && z)
+
+console.log(x || y)
+console.log(z || y)
+
+console.log(!x, !y, !z)
+
+// true
+// false
+
+// true
+// true
+
+// false false true
