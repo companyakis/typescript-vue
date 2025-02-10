77f64033e6e476b5fd0acfd760f8cde10c1a4095
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-10 20:29:17 +0300

    Create part-c-0012-arrays3.ts

---
+++ part-c-0012-arrays3.ts
@@ -0,0 +1,7 @@
+let cities: string[] = [ 'elazığ', 'izmir', 'ankara', 'adana', 'kahramanmaras', 'istanbul' ]
+
+console.log(cities.indexOf("ankara")) // 2
+
+console.log(cities.length) // 6
+
+console.log(cities.includes("samsun")) // false
