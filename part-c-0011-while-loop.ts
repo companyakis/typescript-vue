ca1d00da2afdd5be7e3982e41ddb4e4ad21971f2
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-10 20:09:02 +0300

    Create part-c-0011-while-loop.ts

---
+++ part-c-0011-while-loop.ts
@@ -0,0 +1,16 @@
+let year = 2018
+
+while (year < 2025) {
+
+    console.log(`Year: ${year}`)
+
+    year++
+}
+
+// Year: 2018
+// Year: 2019
+// Year: 2020
+// Year: 2021
+// Year: 2022
+// Year: 2023
+// Year: 2024
