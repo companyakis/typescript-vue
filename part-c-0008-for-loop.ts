baf1c783df3c0f2d07f1ec50cd0cfd2643b8f86e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-10 20:00:39 +0300

    Create part-c-0008-for-loop.ts

---
+++ part-c-0008-for-loop.ts
@@ -0,0 +1,11 @@
+for (let year = 2018; year < 2024; year++) {
+
+    console.log(`Renovation year: ${year}`)
+}
+
+// Renovation year: 2018
+// Renovation year: 2019
+// Renovation year: 2020
+// Renovation year: 2021
+// Renovation year: 2022
+// Renovation year: 2023
