d6d437fdc173c31e39c6cad80c6b2c37429f73bc
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-10 20:07:12 +0300

    Create part-c-0010-for-in-loop.ts

---
+++ part-c-0010-for-in-loop.ts
@@ -0,0 +1,15 @@
+let data = {
+
+    name: "Ayhan",
+    title: "Founder",
+    age: 54
+}
+
+for (let key in data) {
+
+    console.log(`${key} -> ${data[key]}`)
+}
+
+// name -> Ayhan
+// title -> Founder
+// age -> 54
