cc2cf3d82b6e80aa18ce9f5f2a91bcd7de207826
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-13 00:49:16 +0300

    Create part-c-0015-arrays-different-data-types2.ts

---
+++ part-c-0015-arrays-different-data-types2.ts
@@ -0,0 +1,12 @@
+let nested_weekly_sales: (string | number)[][] = [
+    ["week1", "week2", "week3", "week4"],
+    [5200, 4500, 6700, 5697]
+]
+
+console.log(nested_weekly_sales) 
+
+console.log(`Weekly sales example => ${nested_weekly_sales[0][1]} : ${nested_weekly_sales[1][1]}`)
+
+// [ [ 'week1', 'week2', 'week3', 'week4' ], [ 5200, 4500, 6700, 5697 ] ]
+
+// Weekly sales example => week2 : 4500
