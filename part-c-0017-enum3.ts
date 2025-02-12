665d5c48b8db7dee5324d74a92d244679d08ca8c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-13 01:10:50 +0300

    Create part-c-0017-enum3.ts

---
+++ part-c-0017-enum3.ts
@@ -0,0 +1,10 @@
+enum Managers {
+    FinTech = "Mustafa Büyükdereli",
+    Sales = "Aygun Arslan",
+    Operations = "Bilge Göktürk",
+    Accounting = "Hakan Bilir"
+}
+
+console.log(`Our FinTech director is ${Managers.FinTech}`)
+
+//Our FinTech director is Mustafa Büyükdereli
