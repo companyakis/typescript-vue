8620c4caf4e2e63a664016cb357336b05910071b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-13 01:06:23 +0300

    Create part-c-0017-enum1.ts

---
+++ part-c-0017-enum1.ts
@@ -0,0 +1,12 @@
+enum Members {
+    Mustafa,
+    Aygun,
+    Bilge,
+    Hakan
+}
+
+console.log(Members.Mustafa) // 0
+
+console.log(Members.Aygun) // 1
+
+let fintechHead: Members = Members.Mustafa
