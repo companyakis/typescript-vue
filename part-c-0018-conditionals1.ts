97ad53b17053d79e8f1da03d2b1ea3574032dedb
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-13 12:40:10 +0300

    Create part-c-0018-conditionals1.ts

---
+++ part-c-0018-conditionals1.ts
@@ -0,0 +1,11 @@
+let x: number = 21
+
+let y: number = 12
+
+let result1:boolean = x > y ? true : false 
+
+console.log(result1) // true
+
+let result2: string = x > y ? "x is bigger" : "y is bigger"
+
+console.log(result2) // x is bigger
