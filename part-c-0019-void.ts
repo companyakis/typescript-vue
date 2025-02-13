867c38dd9239543e49107368107b0ba1fe5fbcbe
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-13 22:02:56 +0300

    Create part-c-0019-void.ts

---
+++ part-c-0019-void.ts
@@ -0,0 +1,13 @@
+function greeting(name: string) : void {
+
+    console.log(`Hi ${name}!`)
+} 
+
+greeting("Mustafa")
+
+let condition: void = undefined
+
+console.log(condition)
+
+// Hi Mustafa!
+// undefined
