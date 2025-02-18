962fbdfbeece4dfdb2df88d106d453e868630b89
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-18 09:35:31 +0300

    Create part-c-0012-arrays5.ts

---
+++ part-c-0012-arrays5.ts
@@ -0,0 +1,11 @@
+let numbers: number[] = [-11, 14, -3, 99, 121, -142, 10000, 41]
+
+let mappedNumbers = numbers.map((i) => i - 10)
+
+console.log(mappedNumbers) 
+
+// [
+//     -21,   4,  -13,
+//      89, 111, -152,
+//    9990,  31
+//  ]
