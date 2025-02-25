dce17b986cacf656d7be7b79835597b694a9adf6
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-25 14:52:50 +0300

    Create part-c-0016-tuple4.ts

---
+++ part-c-0016-tuple4.ts
@@ -0,0 +1,8 @@
+let emp_aygun: [number, string, string, boolean?] // optional bool
+
+emp_aygun = [621, "Aygün Arslan", "Operations"]
+
+console.log(emp_aygun) // [ 621, 'Aygün Arslan', 'Operations' ]
+
+
+
