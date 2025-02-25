69a44c8f002207b1c071a2ea9821745cd67a168f
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-25 15:33:54 +0300

    Create part-e-0001-sample-mui-project2-appbar-App.jsx

---
+++ part-e-0001-sample-mui-project2-appbar-App.jsx
@@ -0,0 +1,14 @@
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+
+function App() {
+  return (
+    <>
+      <AppBar color="success">
+        <Toolbar sx={{fontSize: 25}}>Important Areas</Toolbar>
+      </AppBar>
+    </>
+  );
+}
+
+export default App;
