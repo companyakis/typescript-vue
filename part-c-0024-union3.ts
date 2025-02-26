b92dce13d422f1a2cfdb659ddf9becdc7ca50e0d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-26 20:39:28 +0300

    Create part-c-0024-union3.ts

---
+++ part-c-0024-union3.ts
@@ -0,0 +1,17 @@
+let data: (string | number)[]
+
+data = [2021, 17, -3.43]
+
+console.log(data)
+
+data = ["mustafa", "aygün"]
+
+console.log(data)
+
+data = [2025, "mustafa"]
+
+console.log(data)
+
+// [ 2021, 17, -3.43 ]
+// [ 'mustafa', 'aygün' ]
+// [ 2025, 'mustafa' ]   
