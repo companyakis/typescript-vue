cb565408f9fb00676d8764b99c7a2c9131500237
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-27 19:53:55 +0300

    Create part-c-0011-while-loop2.ts

---
+++ part-c-0011-while-loop2.ts
@@ -0,0 +1,31 @@
+let num: number = 0
+
+while (num !== 15) {
+  
+    num = Math.floor(Math.random() * 10) + 10
+
+    console.log("Random number: ", num)
+}
+
+console.log("You found it!")
+
+// Random number:  19
+// Random number:  11
+// Random number:  10
+// Random number:  10
+// Random number:  14
+// Random number:  16
+// Random number:  19
+// Random number:  18
+// Random number:  12
+// Random number:  11
+// Random number:  10
+// Random number:  18
+// Random number:  10
+// Random number:  12
+// Random number:  11
+// Random number:  17
+// Random number:  11
+// Random number:  11
+// Random number:  15
+// You found it!
