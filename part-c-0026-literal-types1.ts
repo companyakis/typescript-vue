603874326dbb1d159ecf5750648f0b790a2a46bf
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-03 13:58:16 +0300

    Create part-c-0026-literal-types1.ts

---
+++ part-c-0026-literal-types1.ts
@@ -0,0 +1,11 @@
+type Actions = "go" | "wait" | "stop"
+
+let initialAction: Actions
+
+initialAction = "go"
+
+initialAction = "stop"
+
+initialAction = "wait"
+
+initialAction ="break" // Type '"break"' is not assignable to type 'Actions'
