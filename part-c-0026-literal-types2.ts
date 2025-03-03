0a2946cadfd4e42c1abe7f1f1d43d0dec5954a91
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-03 14:00:33 +0300

    Create part-c-0026-literal-types2.ts

---
+++ part-c-0026-literal-types2.ts
@@ -0,0 +1,11 @@
+type premiumRates = 10 | 15 | 20
+
+let montlyPremium: premiumRates
+
+montlyPremium = 10
+
+montlyPremium = 15
+
+montlyPremium = 20
+
+montlyPremium = 24 // Type '24' is not assignable to type 'premiumRates'
