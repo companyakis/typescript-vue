22afea347adec9eb2c51295b539be03d121c62f1
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-06 10:06:05 +0300

    Create part-d-0007-router3-Header.jsx

---
+++ part-d-0007-router3-Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div className='header'>
+            <Link className='link' to="/">Home</Link>
+            <Link className='link' to="/products">Products</Link>
+            <Link className='link' to="/about">About</Link>
+    </div>
+  )
+}
+
+export default Header
