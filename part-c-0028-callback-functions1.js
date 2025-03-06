404396e9cac00ed763a2fa9563d0c1111c4a164d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-06 12:00:00 +0300

    Create part-c-0028-callback-functions1.js

---
+++ part-c-0028-callback-functions1.js
@@ -0,0 +1,32 @@
+function addOne(x) {
+
+    return x + 1
+}
+
+function addTen(x) {
+
+    return x + 10
+}
+
+let numbers = [3, 7, 10, 4]
+
+function processNumbers(arr, callback) {
+
+    for (let i = 0; i < arr.length; i++) {
+
+        arr[i] = callback(arr[i] * 2)
+    }
+
+    return arr
+}
+
+let result1 = processNumbers(numbers, addOne)
+
+console.log(result1)
+
+let result2 = processNumbers(numbers, addTen)
+
+console.log(result2)
+
+// [ 7, 15, 21, 9 ]
+// [ 24, 40, 52, 28 ]
