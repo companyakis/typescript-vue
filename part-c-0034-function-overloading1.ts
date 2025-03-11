062f13d4be4b35cea7818c484a9a18cde77998b2
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-12 00:58:13 +0300

    Create part-c-0034-function-overloading1.ts

---
+++ part-c-0034-function-overloading1.ts
@@ -0,0 +1,15 @@
+function sum(x: number, y: number) : number 
+
+function sum(x: number, y: number, z: number) : number 
+
+function sum(x: number, y: number, z: number) : number {
+    
+    return x + y + z
+}
+
+let result = sum(4, 12, 456)
+
+console.log(result) // 472
+
+
+export {}
