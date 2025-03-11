5aeb45b8f0978be5e2e140564ba3a46c15e47172
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-11 09:00:36 +0300

    Delete part-d-0005-redux-toolkit3-store.jsx

--- part-d-0005-redux-toolkit3-store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import budgetReducer from '../redux/budgetSlice';
-
-export const store = configureStore({
-    reducer: {
-        budgetCounter: budgetReducer,
-    },
-})
