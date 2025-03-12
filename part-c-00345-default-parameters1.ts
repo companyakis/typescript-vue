634fe330f354c72c32cfca09185ae39719671831
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-12 12:33:17 +0300

    Create part-c-00345-default-parameters1.ts

---
+++ part-c-00345-default-parameters1.ts
@@ -0,0 +1,12 @@
+function multiplication(a: number, b: number, c: number = 1) : number {
+
+    return a * b * c
+}
+
+let result1 = multiplication(3, 4, 5)
+
+console.log(result1) // 60
+
+let result2 = multiplication(30, 40)
+
+console.log(result2) // 1200
