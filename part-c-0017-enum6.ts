fb19bb4649487757b930619088627ad3f4d16e99
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-26 22:00:05 +0300

    Create part-c-0017-enum6.ts

---
+++ part-c-0017-enum6.ts
@@ -0,0 +1,12 @@
+enum EmployeeNumbers {
+
+    Sales = 13,
+    FinTech = 17,
+    Operations = 11,
+    Finance = 3,
+    HR = 2,
+    Marketing = 3,
+    Audit = 1
+}
+
+console.log(EmployeeNumbers.FinTech) // 17
