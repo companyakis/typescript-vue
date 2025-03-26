ca0dd9db60765646c9f5ec7fae8aa8ceec9fa1b9
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-26 23:52:31 +0300

    Create part-c-0026-literal-types4.ts

---
+++ part-c-0026-literal-types4.ts
@@ -0,0 +1,9 @@
+let action: "up" | "down" | "right" | "left"
+
+action = "up"
+
+//action = "lorem" // Type '"lorem"' is not assignable to type '"up" | "down" | "right" | "left"'
+
+let birthYear: 1990 | 1987 | 2014
+
+birthYear = 1990
