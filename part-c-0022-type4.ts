fe1c5f1d761f6d5dffa64cf5b94402ed9a6eb0ae
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-27 11:54:38 +0300

    Create part-c-0022-type4.ts

---
+++ part-c-0022-type4.ts
@@ -0,0 +1,12 @@
+type Employee = "Manager" | "Senior Expert" | "Expert" | "Assistant"
+
+let empAyhan: Employee = "Assistant"
+
+let empSevda: Employee = "Expert"
+
+type UserID = number | string
+
+let idKagan: UserID = 4296
+
+let idSenem: UserID = "1ax654"
+
