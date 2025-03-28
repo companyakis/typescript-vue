ed18f31e423a24990eb362ec0518dfe0d0c4cd52
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-28 14:10:20 +0300

    Create part-c-0007-function7.ts

---
+++ part-c-0007-function7.ts
@@ -0,0 +1,8 @@
+function logUserInfo(name: string, id: string | number) {
+
+    console.log(`User name: ${name} and user ID: ${id}`)
+}
+
+logUserInfo("Ayhan", 4296)
+
+logUserInfo("Hakan", "Ax1254")
