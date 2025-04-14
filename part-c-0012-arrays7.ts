831a7cede1979cf614e8a88a1429973203544275
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-04-14 06:28:52 +0300

    Create part-c-0012-arrays7.ts

---
+++ part-c-0012-arrays7.ts
@@ -0,0 +1,12 @@
+type City = "Ankara" | "İstanbul" | "İzmir" | "Adana"
+
+class Employee {
+
+    name: string
+    title: string
+    id: number | string
+    city: City
+}
+
+let empList: Employee[]
+
